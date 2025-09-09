d5ef5d22198b2295bbcb183d4b68361f23668090
Jeffrey <jeffreyvantillo@gmail.com>
2025-09-09 12:22:10 +0200

    Chapter 13 till 16 finished.

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,7 +1,12 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
- 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Edit Invoice',
+};
+
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
   const id = params.id;
@@ -9,6 +14,10 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+
+  if (!invoice) {
+    notFound();
+  }
 
   return (
     <main>
@@ -25,4 +34,8 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
       <Form invoice={invoice} customers={customers} />
     </main>
   );
+}
+
+function notFound() {
+    throw new Error('Function not implemented.');
 }
